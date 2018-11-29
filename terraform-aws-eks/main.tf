f2b83a9a5817e1c2f8f4d54de207e38f6d114f81
Einsteinish <kihyuck.hong@gmail.com>
2018-11-28 20:57:55 -0800

    initial commit

---
+++ terraform-aws-eks/main.tf
@@ -0,0 +1,12 @@
+# EKS Terraform module
+
+module "eks" {
+  source             = "./modules/eks"
+  cluster-name       = "${var.cluster-name}"
+  aws-region         = "${var.aws-region}"
+  node-instance-type = "${var.node-instance-type}"
+  desired-capacity   = "${var.desired-capacity}"
+  max-size           = "${var.max-size}"
+  min-size           = "${var.min-size}"
+  vpc-subnet-cidr    = "${var.vpc-subnet-cidr}"
+}
---
+++ terraform-aws-eks/variables.tf
@@ -0,0 +1,42 @@
+# Variables Configuration
+
+variable "cluster-name" {
+  default     = "eks-demo-cluster"
+  type        = "string"
+  description = "The name of your EKS Cluster"
+}
+
+variable "aws-region" {
+  default     = "us-east-1"
+  type        = "string"
+  description = "The AWS Region to deploy EKS"
+}
+
+variable "vpc-subnet-cidr" {
+  default     = "10.0.0.0/16"
+  type        = "string"
+  description = "The VPC Subnet CIDR"
+}
+variable "node-instance-type" {
+  default     = "t2.medium"
+  type        = "string"
+  description = "Worker Node EC2 instance type"
+}
+
+variable "desired-capacity" {
+  default     = 1
+  type        = "string"
+  description = "Autoscaling Desired node capacity"
+}
+
+variable "max-size" {
+  default     = 5
+  type        = "string"
+  description = "Autoscaling maximum node capacity"
+}
+
+variable "min-size" {
+  default     = 1
+  type        = "string"
+  description = "Autoscaling Minimum node capacity"
+}
